07df6179ad2c7561a805f5e1e18e87a6e15aea10
Griffin Gradke <griffin.gradke@gmail.com>
2023-02-27 16:32:31 -0600

    add dummy data to dynamically render <ProductItem>

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,25 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {id: 'p1', price: 60, title: 'Mens Casual Tee', description: 'An everyday tee for men'},
+  {id: 'p2', price: 128, title: 'Mens Casual Pant', description: 'Everyday pants for men'},
+  {id: 'p3', price: 40, title: 'Mens Casual Belt', description: 'An everyday belt for men'}
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product => (
+          <ProductItem 
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          /> 
+        ))}
       </ul>
     </section>
   );
